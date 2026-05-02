92d14e096a2c64e4d456add35e17af4749bc1a70
Meenu kumari <meenukumari8792@gmail.com>
2026-05-02 10:17:26 +0530

    fix case sensitive error

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const bcrypt = require("bcryptjs");
 
-const User = require("./models/User");
+const User = require("./models/user");
 const Task = require("./models/Task");
 const PORT = process.env.PORT || 5000;
 
