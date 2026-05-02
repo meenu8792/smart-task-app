ea1aedd524da74dc40df6a06eaecb678f35138d7
Meenu kumari <meenukumari8792@gmail.com>
2026-05-02 10:25:23 +0530

    add root route

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,9 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.get("/", (req, res) => {
+  res.send("Backend is running 🚀");
+});
 
 // DB
 mongoose.connect(process.env.MONGO_URI)
